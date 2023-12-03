01342bc9dcf1dd7c7c42de0ecfe530f3d645dbd1
Ch-Praneeth1 <chpraneethvardhanreddy143@gmail.com>
2023-12-03 23:40:37 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <h1 className='text-red-500'>fnrf</h1>
   );
-}
+};
+
+
+
+//  container
+//    header
+//    Body
+//      sidebar
+//         menu
+//      body container
+//          buttons list
+//          video container
+//              video cards
+
 
 export default App;
